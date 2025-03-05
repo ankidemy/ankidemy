9578ab889f148f06b7615b4fa7f9674f0b15f0cf
fernandocruzp <cruzfernando@ciencias.unam.mx>
2025-03-05 06:25:31 -0600

    Ya está el esquema

--- db/init-scripts/init-sql.sql
+++ db/init-scripts/init-sql.sql
@@ -1,6 +1,96 @@
 -- db/init-scripts/init-sql.sql
 -- Initialize database schema
 
+--Crea tabla usuarios
+CREATE TABLE IF NOT EXISTS usuario(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    contrasena VARCHAR(255) NOT NULL,
+    nivel VARCHAR(20),
+    nombre VARCHAR(30),
+    apellido VARCHAR(100)
+);
+
+--Crea tabla temario
+CREATE TABLE IF NOT EXISTS temario (
+    id_temario SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    privacidad VARCHAR(20) CHECK (privacidad IN ('publico', 'privado')) NOT NULL,
+    dueno INT NOT NULL,
+    FOREIGN KEY (dueno) REFERENCES usuario(id) ON DELETE CASCADE
+);
+
+--Crea tabla de estudiantes_temario
+CREATE TABLE IF NOT EXISTS estudiantes_temario(
+    usuario_id INT NOT NULL,
+    temario_id INT NOT NULL,
+    fecha_inscripcion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    avance NUMERIC(5,2) CHECK (avance BETWEEN 0 AND 100),
+    PRIMARY KEY (usuario_id, temario_id),
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id) ON DELETE CASCADE,
+    FOREIGN KEY (temario_id) REFERENCES temario(id_temario) ON DELETE CASCADE
+);
+
+--Crea tabla comentarios_temario
+CREATE TABLE IF NOT EXISTS comentarios_temario (
+    id_comentario SERIAL PRIMARY KEY,
+    contenido VARCHAR(1000) NOT NULL,
+    id_temario INT NOT NULL,
+    FOREIGN KEY (id_temario) REFERENCES temario(id_temario) ON DELETE CASCADE
+);
+
+--Crea tabla tema
+CREATE TABLE IF NOT EXISTS tema (
+    id_tema SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    info VARCHAR(100000) NOT NULL,
+    dueno INT NOT NULL,
+    id_temario INT NOT NULL,
+    FOREIGN KEY (dueno) REFERENCES usuario(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_temario) REFERENCES temario(id_temario) ON DELETE CASCADE
+);
+
+--Crea tabla jerarquie_temas
+CREATE TABLE IF NOT EXISTS jerarquia_temas(
+    padre_id INT NOT NULL,
+    hijo_id INT NOT NULL,
+    PRIMARY KEY (padre_id, hijo_id),
+    FOREIGN KEY (padre_id) REFERENCES tema(id_tema) ON DELETE CASCADE,
+    FOREIGN KEY (hijo_id) REFERENCES tema(id_tema) ON DELETE CASCADE
+);
+
+--Crea tabla estudiantes_tema
+CREATE TABLE IF NOT EXISTS estudiantes_tema(
+    usuario_id INT NOT NULL,
+    tema_id INT NOT NULL,
+    ultima_fecha_repaso TIMESTAMP,
+    avance NUMERIC(5,2) CHECK (avance BETWEEN 0 AND 100),
+    contador INT,
+    PRIMARY KEY (usuario_id, tema_id),
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id) ON DELETE CASCADE,
+    FOREIGN KEY (tema_id) REFERENCES tema(id_tema) ON DELETE CASCADE
+);
+
+--Crea tabla ejercicios
+CREATE TABLE IF NOT EXISTS ejercicio(
+    id_ejercicio SERIAL PRIMARY KEY,
+    tema_id INT NOT NULL,
+    dificultad VARCHAR(20) CHECK (dificultad IN ('facil', 'moderado','dificil')) NOT NULL,
+    contenido JSONB,
+    FOREIGN KEY (tema_id) REFERENCES tema(id_tema) ON DELETE CASCADE
+);
+
+--Crea tabla material
+CREATE TABLE IF NOT EXISTS material(
+    id_material SERIAL PRIMARY KEY,
+    tema_id INT NOT NULL,
+    tipo VARCHAR(20),
+    contenido BYTEA NOT NULL,
+    FOREIGN KEY (tema_id) REFERENCES tema(id_tema) ON DELETE CASCADE
+);
+
+
 -- Create users table
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
